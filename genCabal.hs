3ae53500bf55188b19f4c6118ff0710678ad9659
Omari Norman <omari@smileystation.com>
2015-11-20 22:41:26 -0500

    WIP

---
+++ genCabal.hs
@@ -0,0 +1,65 @@
+#!/usr/bin/env stack
+-- stack --resolver=lts-3.14 --install-ghc runghc --package=cartel
+
+module Main where
+
+import Cartel
+
+pinchotVer :: [Word]
+pinchotVer = [0,1,0,0]
+
+atleast :: NonEmptyString -> Version -> Package
+atleast n v = package n (gtEq v)
+
+base :: Package
+base = closedOpen "base" [4,8,0,0] [5]
+
+commonOptions :: HasBuildInfo a => [a]
+commonOptions =
+  [ ghcOptions ["-W"]
+  , haskell2010
+  , hsSourceDirs ["lib"]
+  ]
+
+libraryDepends :: [Package]
+libraryDepends = [ base ]
+
+props :: Properties
+props = blank
+  { name = "pinchot"
+  , version = pinchotVer
+  , cabalVersion = Just (1,14)
+  , buildType = Just simple
+  , license = Just bsd3
+  , licenseFile = "LICENSE"
+  , copyright = "2015 Omari Norman"
+  , author = "Omari Norman"
+  , maintainer = "omari@smileystation.com"
+  , stability = "Experimental"
+  , homepage = "http://www.github.com/massysett/pinchot"
+  , bugReports = "http://www.github.com/massysett/pinchot/issues"
+  , synopsis = "Build parsers and ASTs for context-free grammars"
+  , extraSourceFiles = ["README.md"]
+  , description =
+    [ "Pinchot builds parsers and ASTs for context-free grammars."
+    , ""
+    , "For more information, please see the README.md file, which"
+    , "is available in the source tarball or is visible at the bottom"
+    , "of the Pinchot homepage:"
+    , ""
+    , "<http://www.github.com/massysett/pinchot>"
+    ]
+  , category = "Development"
+  }
+
+main :: IO ()
+main = defaultMain $ do
+  libMods <- modules "lib"
+  return
+    ( props
+    ,   exposedModules libMods
+      : buildDepends libraryDepends
+      : commonOptions
+    , [ githubHead "massysett" "penny"
+      ]
+    )
